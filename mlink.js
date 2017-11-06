21cc466692a7eb37f334a634c5c1b5c387fd5d4b
fr0gs <elf_elegua@yahoo.com>
2017-11-06 22:27:47 +0100

    Basic linking & unlinking functionality done

--- mlink.js
+++ mlink.js
@@ -4,9 +4,11 @@
 
 const program = require('commander');
 const fs = require('fs');
+const process = require('process');
 const paths = require('global-paths');
 
 const NPM_GLOBAL_PATH = "/usr/lib/node_modules";
+const PROJECT_PATH = process.cwd();
 
 
 function readConfigFile() {
@@ -46,7 +48,30 @@ program.parse(process.argv);
 
 
 function removeLinks(modules={}) {
+    if (isEmptyObject(modules)) {
+        throw new Error("The mlink.config.json file is empty")
+    }
+    if (paths().indexOf(NPM_GLOBAL_PATH) != -1) {
+        Object.keys(modules).forEach((module) => {
+            const global_module_path = `${NPM_GLOBAL_PATH}/${module}`;
+            const local_module_path = `${PROJECT_PATH}/node_modules/${module}`;
+            const repo_module_path = `${modules[module].path}`
 
+            if (fs.existsSync(local_module_path)) {
+                const stats = fs.lstatSync(local_module_path);
+                if (stats.isSymbolicLink()) {
+                    console.log(`[+] Remove local link from ${local_module_path} -> ${global_module_path}`);
+                    fs.unlinkSync(local_module_path);
+                }
+            }
+
+            if (fs.existsSync(global_module_path)) {
+                console.log(`[+] Remove global link from ${global_module_path} -> ${repo_module_path}`);
+                fs.unlink(global_module_path);
+            }
+        });
+    }
+    else throw new Error("/usr/lib/node_modules does not exist");    
 }
 
 function linkModules(modules={}) {
@@ -56,7 +81,18 @@ function linkModules(modules={}) {
     else {
         if (paths().indexOf(NPM_GLOBAL_PATH) != -1) {
             Object.keys(modules).forEach((module) => {
-                fs.symlinkSync(modules[module].path, `${NPM_GLOBAL_PATH}/${module}`);
+                const global_module_path = `${NPM_GLOBAL_PATH}/${module}`;
+                const local_module_path = `${PROJECT_PATH }/node_modules/${module}`;
+                const repo_module_path = `${modules[module].path}`;
+
+                if (fs.existsSync(local_module_path)) {
+                    console.log(`[+] Path ${local_module_path} already exists, removing it.`);
+                    fs.unlink(local_module_path);
+                }
+                console.log(`[+] Create link from ${global_module_path} -> ${repo_module_path}`);
+                fs.symlinkSync(repo_module_path, global_module_path);
+                console.log(`[+] Create link from ${local_module_path} -> ${global_module_path}`);
+                fs.symlinkSync(global_module_path, local_module_path);
             });
         }
         else throw new Error("/usr/lib/node_modules does not exist");
